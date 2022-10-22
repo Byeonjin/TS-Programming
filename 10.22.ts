a6782e223aa13250c3c610ea50d35aeac68ceb11
Byeon Jinsang <quswlstkd88@naver.com>
2022-10-22 16:57:14 +0900

    10.22

---
+++ 10.22.ts
@@ -0,0 +1,22 @@
+//1. ESNext 특징
+
+//(4) 제너릭
+/* 
+class Container<T> {
+    constructor(public value: T){
+
+    }
+}
+
+const number:Container<number> = new Container<number>(1)
+const str:Container<string> = new Container<string>("hello world")
+ */
+
+//
+
+// (5) 대수타입
+/* 
+type NumberOrString = number | string
+type AnimalAndPerson = Animal & Person
+ */
+
